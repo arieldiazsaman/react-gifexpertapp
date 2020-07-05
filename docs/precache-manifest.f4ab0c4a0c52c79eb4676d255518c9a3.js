f800c0f628e480289b206c7068bbcc94f28e72e6
Ariel Diaz <arieldiazsamaniego@gmail.com>
2020-07-05 15:05:35 -0400

    docs added

---
+++ docs/precache-manifest.f4ab0c4a0c52c79eb4676d255518c9a3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ffcf05e7c435e289fe58e21967f2e61e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "53f2745c73058c65a024",
+    "url": "/static/css/main.344f7a4c.chunk.css"
+  },
+  {
+    "revision": "85a4a86f3fe06f17fcc7",
+    "url": "/static/js/2.8249eaeb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.8249eaeb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "53f2745c73058c65a024",
+    "url": "/static/js/main.a85873a8.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
